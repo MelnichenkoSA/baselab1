9a826b583a3723affbe71b432e6be26071a269dc
melnichenkoSA <melnichenkoSA@313D-10>
2022-09-03 10:30:42 +0700

    jgbcfybt

--- baselab1/Program.cs
+++ baselab1/Program.cs
@@ -1,4 +1,25 @@
 ﻿using System;
 
-Console.WriteLine("Hello World!");
-<LangVersion>preview
+int X;
+
+int Y;
+
+Console.WriteLine("Введите число Х");
+X = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число Y");
+Y = Convert.ToInt32(Console.ReadLine());
+
+//int c = X;
+
+//X = Y;
+
+//Y = c;
+
+X = X - Y;
+
+Y = Y + X;
+
+X = -(X - Y);
+
+Console.WriteLine($"X = {X}, Y = {Y}");
